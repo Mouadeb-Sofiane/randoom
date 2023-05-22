f7a7ee6e9b855e24ba2850913f69e7ddbf7b519f
Sofiane-Mouadeb <sofiane.mouadeb@edu.univ-fcomte.fr>
2023-05-22 19:09:54 +0200

    recpommencement du code de 0

--- randoom/src/App.vue
+++ randoom/src/App.vue
@@ -2,7 +2,6 @@
 	import { RouterView } from 'vue-router'
   import Header from './components/HeaderPage.vue'
   import Footer from './components/FooterPage.vue'
-  import PocketBase from 'pocketbase'
 </script>
 
 <template>
