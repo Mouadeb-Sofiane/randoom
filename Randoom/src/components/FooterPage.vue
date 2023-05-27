960eb34071e08ecdd82c66d8ed736cb9c5e6f600
Sofiane-Mouadeb <sofiane.mouadeb@edu.univ-fcomte.fr>
2023-05-27 16:19:04 +0200

    footer + accueil début

--- Randoom/src/components/FooterPage.vue
+++ Randoom/src/components/FooterPage.vue
@@ -1,7 +1,28 @@
 <template>
-    <footer>
-        <div>
-            <p>footer</p>
+    <header>
+    <nav class="fixed bottom-0 w-screen bg-barmenu border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+            <a href="#" class="flex justify-between ">
+                <img src="src/assets/icon/home-black.svg" class="mr-3 h-6 sm:h-9" alt="home color black" />
+                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"></span>
+            </a>
+            <a href="#" class="flex justify-between ">
+                <img src="src/assets/icon/search-black.svg" class="mr-3 h-6 sm:h-9" alt="search color black" />
+                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"></span>
+            </a>
+            <a href="#" class="flex justify-between ">
+                <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Logo" />
+                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"></span>
+            </a>
+            <a href="#" class="flex justify-between ">
+                <img src="src/assets/icon/list-black.svg" class="mr-3 h-6 sm:h-9" alt="list color black" />
+                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"></span>
+            </a>
+            <a href="#" class="flex justify-between ">
+                <img src="src/assets/icon/profile-black.svg" class="mr-3 h-6 sm:h-9" alt="profile color black" />
+                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"></span>
+            </a>
         </div>
-    </footer>
+    </nav>
+</header>
 </template>
--- Randoom/src/pages/index.vue
+++ Randoom/src/pages/index.vue
@@ -1,3 +1,27 @@
 <template>
-  <h1>Page d'accueil</h1>
+  <h1>Bienvenue !</h1>  
+<p>Si vous ne savez pas quoi choisir comme divertissement la roue choisira à votre place !</p>
+<h2 class="border-b-4 border-b-black p-8 m-8">Inspiré par RANDOOM</h2>
+<h3>Films et Séries</h3>
+<h3 class="border-b-4 border-b-black p-2 m-3 w-40">Films et Séries</h3>
+<div class="carousel carousel-center rounded-box">
+<div class="carousel-item">
+  <div class="border-b-black flex flex-col justify-start items-start w-[185px] h-[278px] relative overflow-hidden bg-white">
+  <div class="flex-grow-0 flex-shrink-0 w-[185px] h-[167px] relative overflow-hidden bg-[#dedede]"></div>
+  <div class="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 h-[111px] w-[185px] relative gap-1.5 pl-2.5 pr-[11px] pt-[21px] pb-6">
+  <p class="flex-grow-0 flex-shrink-0 w-32 text-[13px] text-left text-black">Headline</p>
+  <p class="flex-grow-0 flex-shrink-0 w-[168px] text-[13px] text-left text-[#666]">
+    Please add your content here. Keep it short and simple. And smile :
+  </p></div>
+</div>
+</div> 
+
+<h3>Livres et Podcast</h3>
+<h2>Vos Films et séries préférées</h2>
+<h2>Livres et podcast populaires</h2>
+
+</div>
+<h3 class="border-b-4 border-b-black p-2 m-3 w-40">Livres et Podcast</h3>
+<h2 class="border-b-4 border-b-black p-2 m-3 w-60">Vos Films et séries préférées</h2>
+<h2 class="border-b-4 border-b-black p-2 m-3 w-60">Livres et podcast populaires</h2>
 </template>
