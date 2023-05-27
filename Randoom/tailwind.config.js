85490aa4204d739f37928e174c9b3f5a8954316c
Sofiane-Mouadeb <sofiane.mouadeb@edu.univ-fcomte.fr>
2023-05-27 15:55:38 +0200

    installation et configuration de vue et tailwind

---
+++ Randoom/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      white: '#ffffff',
+      black: '#000000',
+      barmenu: '#B9CCD3',
+      fond: '#DBEDF3',
+      transparent: 'transparent',
+      gray: colors.zinc
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
+    extend: {
+      gridTemplateColumns: {
+        principal: 'repeat(12, minmax(0, 64px))'
+      }
+    }
+  },
+  plugins: []
+}
+
