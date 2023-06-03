1d065e61878a607fe731c454857703291c26b29d
Sofiane-Mouadeb <sofiane.mouadeb@edu.univ-fcomte.fr>
2023-06-03 12:37:34 +0200

    page connection and footer

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -1,31 +1,3 @@
-<!-- <template>
-    <header>
-    <nav class="fixed bottom-0 w-screen bg-barmenu border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="#" class="flex justify-between ">
-                <img src="#" class="mr-3 h-6 sm:h-9" alt="home color black" />
-                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"></span>
-            </a>
-            <a href="#" class="flex justify-between ">
-                <img src="#" class="mr-3 h-6 sm:h-9" alt="search color black" />
-                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"></span>
-            </a>
-            <a href="#" class="flex justify-between ">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Logo" />
-                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"></span>
-            </a>
-            <a href="#" class="flex justify-between ">
-                <img src="#" class="mr-3 h-6 sm:h-9" alt="list color black" />
-                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"></span>
-            </a>
-            <a to="/connexion" class="flex justify-between ">
-                <img src="#" class="mr-3 h-6 sm:h-9" alt="profile color black" />
-                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"></span>
-            </a>
-        </div>
-    </nav>
-</header>
-</template> -->
 <script setup lang="ts">
 	import { ref, computed, watch } from 'vue'
     import LogoIcon from '@/components/icons/IconLogo.vue'
@@ -33,33 +5,28 @@
 </script>
 
 <template>
-  <header class="">
-    <div class="flex items-center">
-      <a href="/">
-      <LogoIcon/>
-      </a>
-    </div>
+  <footer>
 
     <nav class="fixed bottom-0 w-screen bg-barmenu border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-  <ul class="mt-[25vh] ml-16 font-bold font-size lg:m-0 lg:font-bold lg:text-3xl lg:flex lg:gap-14 lg:pr-20">
-    <li class="menu-item">
+  <ul class="flex justify-between">
+    <li class="menu-item self-center text-xl font-semibold whitespace-nowrap dark:text-white">
 		<RouterLink class="menu-link font-serif" to="/billetterie" >Billetterie</RouterLink>
 	</li>
-    <li class="menu-item">
+    <li class="menu-item self-center text-xl font-semibold whitespace-nowrap dark:text-white">
 		<RouterLink class="menu-link font-serif" to="/oeuvre" >Oeuvres</RouterLink>
     </li>
-    <li class="menu-item">
+    <li class="menu-item self-center text-xl font-semibold whitespace-nowrap dark:text-white">
 		<RouterLink class="menu-link font-serif" to="/"><LogoIcon/></RouterLink>
 	</li>
-    <li class="menu-item">
+    <li class="menu-item self-center text-xl font-semibold whitespace-nowrap dark:text-white">
 		<RouterLink class="menu-link font-serif" to="/artiste" >Artistes</RouterLink>
 	</li>
-    <li class="menu-item">
+    <li class="menu-item self-center text-xl font-semibold whitespace-nowrap dark:text-white">
 		<RouterLink class="menu-link font-serif" to="/connexion" >Connection</RouterLink>
 	</li>
   </ul>
     </div>
     </nav>
-  </header>
+</footer>
 </template>
--- src/pages/connexion.vue
+++ src/pages/connexion.vue
@@ -1,131 +1,3 @@
-<!-- <script setup lang="ts">
-import { onMounted, ref } from 'vue'
-import PocketBase from 'pocketbase'
-
-
-var pocketbase_ip=''
-if (import.meta.env.MODE === 'production')
-  pocketbase_ip="http://185.216.26.50:80"
-else 
-  pocketbase_ip="http://127.0.0.1:8090"
-alert (pocketbase_ip)
-
-let pb:any = null;
-const currentUser = ref();
-const username = ref("");
-const password = ref("");
-const fullName = ref("");
-
-const loginMode = ref(true);
-
-onMounted(async() => {
-    pb = new PocketBase('http://127.0.0.1:8090');
-
-    pb.authStore.onChange(() =>{
-        currentUser.value = pb.authStore.model
-    },true)
-});
-
-const doLogout = () => {
-    pb.authStore.clear();
-    // currentUser.value = null;
-}
-
-const doLogin = async () => {
-    try{
-    const authData = await pb.collection('users')
-    .authWithPassword(username.value, password.value);
-
-  console.log(pb.authStore.isValid);
-  console.log(pb.authStore.token);
-  console.log(pb.authStore.model);
-//   currentUser.value = pb.authStore.model
-} catch (error){
-    alert(error.message)
-}
-}
-
-const doCreateAccount = async () => {
-    try{
-    const data = {
-        "username": `user_${self.crypto.randomUUID().split("_")[0]}`,
-        "email": username.value,
-        "emailVisibility": true,
-        "password":password.value,
-        "passwordConfirm": password.value,
-        "name": fullName.value,
-
-    };
-
-
-    const record = await pb.collection('users').create(data);
-
-    await doLogin();
-  } catch (error) {
-    alert(error.message)
-  }
-
-}
-
-
-</script>
-<template>
-    <div class="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
-        <div class="w-full max-w-md space-y-8">
-        <div v-if="currentUser">    
-            <h1>Welcome {{ currentUser?.name }}</h1>
-            <div>
-                <button type="button" @click="$event => doLogout" class="rounded-md bg-indigo-800 px-3 py-2 text-sm text-black shadow-sm hover:bg-btnfond2 focus-visible:outline-btnfond" >
-                    logout</button>
-            </div>
-        </div>
-            <div v-else>
-                <h1>{{ loginMode ? "Se connecter":"S'inscire" }}</h1> 
-                <div class="sm:col-span-2 sm:col-start-1 mt-4">
-                    <label for="username" class="block text-sm text-black">Adresse mail</label>
-                <div class="mt-2">
-                    <input v-model="username" type="text" name="username" id="username" autocomplete="none" placeholder="Adresse mail"
-                    class="px-2 block w-full rounded-md border-0 py-1.5 text-black shadow-sm ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2">
-                </div>
-                </div>
-                <div class="sm:col-span-2 sm:col-start-1 mt-2">
-                    <label for="password" class="block text-sm text-black">Mot de passe</label>
-                <div class="mt-2">
-                    <input v-model="password" type="password" name="password" id="password" autocomplete="none" placeholder="Mot de passe"
-                    class="px-2 block w-full rounded-md border-0 py-1.5 text-black shadow-sm ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2">
-                </div>
-                </div> 
-                
-                <div v-if="loginMode">
-                <div class="sm:col-span-2 sm:col-start-1 mt-2">
-                <button type="button" @click = "doLogin" class="mr-5 rounded-md bg-indigo-800 px-3 py-2 text-sm text-black shadow-sm hover:bg-indigo-500 focus-visible:outline-btnfond" >
-                    Se connecter</button>
-
-                <button type="button" @click="$event => loginMode=false" class="rounded-md bg-indigo-800 px-3 py-2 text-sm text-blackblack shadow-sm hover:bg-indigo-500 focus-visible:outline-btnfond" >
-                    S'inscrire</button>
-                </div>          
-            </div>
-            <div v-else>
-                <div class="sm:col-span-2 sm:col-start-1 mt-2">
-                    <label for="FullName" class="block text-sm text-black">Nom</label>
-                <div class="mt-2">
-                    <input v-model="fullName" type="text" name="fullName" id="Fullname" autocomplete="none" placeholder="Nome"
-                    class="px-2 block w-full rounded-md border-0 py-1.5 text-black shadow-sm ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2">
-                </div>
-                </div> 
-                <div class="sm:col-span-2 sm:col-start-1 mt-2">
-                <button type="button" @click = "doCreateAccount" class="mr-5 rounded-md bg-indigo-800 px-3 py-2 text-sm text-black shadow-sm hover:bg-indigo-500 focus-visible:outline-btnfond" >
-                    S'inscrire</button>
-
-                <button type="button" @click="$event => loginMode=true" class="rounded-md bg-red-600 px-3 py-2 text-sm text-black shadow-sm hover:bg-red-indigo-500 focus-visible:outline-btnfond" >
-                    Annuler</button>
-                </div>    
-            </div>
-        </div>
-    </div>
-    </div>
-</template>  -->
-
 <template>
     <div class="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
       <div class="w-full max-w-md space-y-8">
@@ -138,7 +10,7 @@ const doCreateAccount = async () => {
           </div>
         </div>
         <div v-else>
-          <h1>{{ loginMode ? "LOGIN" : "CREATE ACCOUNT" }}</h1>
+          <h1>{{ loginMode ? "Se connecter" : "S'inscrire" }}</h1>
           <div class="sm:col-span-2 sm:col-start-1 mt-4">
             <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Adresse mail</label>
             <div class="mt-2">
@@ -158,7 +30,8 @@ const doCreateAccount = async () => {
           <div v-if="loginMode">
             <div class="sm:col-span-2 sm:col-start-1 mt-2">
               <button type="button" @click="doLogin"
-                class="mr-3 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-black shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Se connecter</button>
+                class="mr-3 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-black shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                Se connecter</button>
   
               <button type="button" @click="loginMode = false"
                 class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-black shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
