d74aebb701b01d96d32a4e550a7b1d83270b2120
Sofiane-Mouadeb <sofiane.mouadeb@edu.univ-fcomte.fr>
2023-06-05 08:56:50 +0200

    import des fichier json

--- pocketbase/import.mjs
+++ pocketbase/import.mjs
@@ -1,6 +1,7 @@
 import {pb} from "./backend.mjs"
 import books from "./books.json" assert { type: 'json' };
-import books from "./series.json" assert { type: 'json' };
+import series from "./series.json" assert { type: 'json' };
+import film from "./film.json" assert { type:'json' };
 
 for (const aBook of books) {
     //console.log(aBook);
@@ -11,3 +12,9 @@ for (const aSerie of series) {
     //console.log(aBook);
     pb.collection("serie").create(aSerie)
 }
+
+for (const aFilm of film) {
+    //console.log(aBook);
+    pb.collection("film").create(aFilm)
+}
+
