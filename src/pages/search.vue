29cc6a0aa97eb6620e45c0026ad7b44e7df4b5f2
Sofiane-Mouadeb <sofiane.mouadeb@edu.univ-fcomte.fr>
2023-06-06 15:22:37 +0200

    roue random film fonctionnelle

--- src/pages/search.vue
+++ src/pages/search.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 import { pb } from '@/backend'
 import type { FilmResponse } from '@/pocketbase-types';
 import CardFilms from '@/components/CardFilms.vue';
@@ -24,7 +24,35 @@ fetchFilmList();
 
 <template>
     <div class="mt-40">
-        <button @click="displayRandomFilm" class="text-black">Cliquez pour avoir toutes les information concernant le film.</button>
+        <button @click="displayRandomFilm" class="text-black">Cliquez pour avoir toutes les information concernant le film.</button> 
         <CardFilms v-if="showRandomFilm" v-bind="{ ...RandomFilm }" />
     </div>
+</template> -->
+
+<script setup lang="ts">
+import { pb } from '@/backend'
+import type { FilmResponse } from '@/pocketbase-types';
+import CardFilms from '@/components/CardFilms.vue';
+import { ref, onMounted } from 'vue';
+
+let listefilm: FilmResponse[] = [];
+let RandomFilm: FilmResponse | null = null;
+const showRandomFilm = ref(false);
+
+async function fetchFilmList() {
+  listefilm = await pb.collection('film').getFullList<FilmResponse>();
+  RandomFilm = listefilm[Math.floor(Math.random() * listefilm.length)];
+  console.log("Tirer un film aléatoirement", RandomFilm);
+  showRandomFilm.value = true; // Afficher les informations sur le film une fois récupérées
+}
+
+onMounted(() => {
+  fetchFilmList();
+});
+</script>
+
+<template>
+  <div class="mt-40">
+    <CardFilms v-if="showRandomFilm" v-bind="{ ...RandomFilm }" />
+  </div>
 </template>
