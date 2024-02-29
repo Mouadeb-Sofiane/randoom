ac1ae1829e36904b3f06d5c1f9f75f5949c2dd69
Mouadeb-Sofiane <116799161+Mouadeb-Sofiane@users.noreply.github.com>
2024-02-29 17:08:41 +0100

    dernier commir push

--- src/pages/[...404].vue
+++ src/pages/[...404].vue
@@ -4,11 +4,26 @@
 </script>
 
 <template>
-    <p class="pt-96"></p>
-    <div>
-        <h1 class="text-center text-4xl font-bold mt-20 mb-20">Erreur 404</h1>
-        <p class="text-center text-4xl">Désolé, la page que vous recherchez est introuvable. Veuillez renseignez une adresse valide.</p>
-        <img src="" alt="" class="mx-auto w-80 ">
+    <div class="bg-indigo-900 relative overflow-hidden h-screen">
+        <img src="https://external-preview.redd.it/4MddL-315mp40uH18BgGL2-5b6NIPHcDMBSWuN11ynM.jpg?width=960&crop=smart&auto=webp&s=b98d54a43b3dac555df398588a2c791e0f3076d9" class="absolute h-full w-full object-cover"/>
+    <div class="inset-0 bg-black opacity-25 absolute">
     </div>
-    <p class="pb-96"></p>
+    <div class="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
+        <div class="w-full font-mono flex flex-col items-center relative z-10">
+            <h1 class="font-extrabold text-3xl text-center text-white leading-tight mt-4">
+                Désolé, la page que vous recherchez est introuvable.
+            </h1>
+            <br>
+            <br>
+            <br>
+            <h1 class="font-extrabold text-xl text-center text-white leading-tight mt-4">
+                Cliquez sur le logo pour retrouver votre chemin.
+            </h1>
+            <p class="font-extrabold text-8xl my-44 text-white animate-bounce">
+                Erreur 404
+            </p>
+        </div>
+    </div>
+    </div>
+    <p class="pb-4"></p>
 </template>
