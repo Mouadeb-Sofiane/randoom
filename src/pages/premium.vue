6eade5f4cea42430da16ff50be4db1743fd22690
Mouadeb-Sofiane <116799161+Mouadeb-Sofiane@users.noreply.github.com>
2024-02-29 17:45:23 +0100

    premium

--- src/pages/premium.vue
+++ src/pages/premium.vue
@@ -1,11 +1,11 @@
-
+<template>
 <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
     <h5 class="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">Standard plan</h5>
-    <div class="flex items-baseline text-gray-900 dark:text-white">
-        <span class="text-3xl font-semibold">$</span>
-        <span class="text-5xl font-extrabold tracking-tight">49</span>
-        <span class="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">/month</span>
-    </div>
+        <div class="flex items-baseline text-gray-900 dark:text-white">
+            <span class="text-3xl font-semibold">$</span>
+            <span class="text-5xl font-extrabold tracking-tight">49</span>
+            <span class="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">/month</span>
+        </div>
     <!-- List -->
     <ul role="list" class="space-y-5 my-7">
         <li class="flex space-x-3">
@@ -45,4 +45,5 @@
         </li>
     </ul>
     <button type="button" class="text-white bg-blue hover:bg-blue focus:ring-4 focus:outline-none focus:ring-blue dark:focus:ring-blue font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">Choisir cet abonnement</button>
-</div>
+    </div>    
+</template>
