b0da05f977f2f18c75ad2f86cfa70a3301955715
Mouadeb-Sofiane <116799161+Mouadeb-Sofiane@users.noreply.github.com>
2024-02-29 17:49:04 +0100

    prem

--- src/pages/premium.vue
+++ src/pages/premium.vue
@@ -47,3 +47,8 @@
     <button type="button" class="text-white bg-blue hover:bg-blue focus:ring-4 focus:outline-none focus:ring-blue dark:focus:ring-blue font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">Choisir cet abonnement</button>
     </div>    
 </template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import type { SeriesResponse } from '@/pocketbase-types';
+</script>
