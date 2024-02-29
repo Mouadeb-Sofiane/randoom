d106acf420503e2b20925cc4e5adad8ee7efd2e5
Mouadeb-Sofiane <116799161+Mouadeb-Sofiane@users.noreply.github.com>
2024-02-29 17:10:56 +0100

    last

---
+++ src/pages/JsinData.vue
@@ -0,0 +1,18 @@
+<template>
+  
+    <div class="m-5">
+      <p class="font-serif text-3xl">Titre du film : {{ Series_Title }}</p>
+      <p class="font-serif text-3xl">date de création : {{ Poster_Link }}</p>
+    <p>{{ Released_Year }}</p>
+    <p>{{ Runtime }}</p>
+    <p>{{ Genre }}</p>
+    <p>{{ Overview }}</p>
+    <p>{{ Director }}</p>
+    <p>{{ Star1 }}</p>
+    <p>{{ Star2 }}</p>
+    <p>{{ Star3 }}</p>
+    <p>{{ Star4 }}</p>
+    </div>
+  </template>
+
+
